bb13e82ede633c1c12fa5b4b235cd6041b6e5ba2
Christopher Dent <christopherdent01@gmail.com>
2019-12-14 01:25:44 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,15 +11,18 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-     @user = User.find_by(username: params[:email])
+
+    @user = User.find_by(username: params["username"])
     if @user && @user.password == params[:password]
       session[:user_id] = @user.id
       redirect to '/account'
-     
-    else 
-      redirect to '/error'
-    end 
-  end
+    else
+      erb :error
+    end
+  end	
+  
+  
+  
 
   get '/account' do
     erb :account 
