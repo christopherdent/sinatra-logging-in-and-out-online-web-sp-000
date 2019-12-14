171d6eee5a200b1654487ede01e62bff22295ac0
Christopher Dent <christopherdent01@gmail.com>
2019-12-14 01:24:22 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,25 +1,11 @@
 class Helpers
+  def self.current_user(session_hash)
+    User.find(session_hash["user_id"])
+  end
 
+  def self.is_logged_in?(session_hash)
+    !!session_hash["user_id"]
+  end
+end 	 
 
-
-  def current_user(session_hash)
-    session[:user_id] = @user.id
-    if session_hash = @user.id 
-      return @user.id 
-    end 
-    
-    
-  end 
-
-
-  def is_logged_in?(session_hash)
-    !!session_hash[user_id]
-  end 
-
-
-
-
-
-
-
-end
+ 
