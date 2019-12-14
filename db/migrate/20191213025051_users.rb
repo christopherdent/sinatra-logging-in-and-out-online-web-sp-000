f5c5c0b0c545e9e40a53d088a2dd829fc3b9733a
Christopher Dent <christopherdent01@gmail.com>
2019-12-14 02:02:39 +0000

    Automatically backed up by Learn

--- db/migrate/20191213025051_users.rb
+++ db/migrate/20191213025051_users.rb
@@ -1,7 +1,7 @@
 class Users < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.string :user_id
+      t.string :username
       t.string :password 
       t.float :balance
     end
