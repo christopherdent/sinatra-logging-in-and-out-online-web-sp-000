ff1637857160f4336ddf695b553ac01b5a3f0f44
Christopher Dent <christopherdent01@gmail.com>
2019-12-14 01:35:44 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,6 @@
 class User 
   attr_accessor :username, :password, :balance
   
-      create_table :users do |t|
-      t.string :username
-      t.string :password
-      t.float :balance
-      
-    end 
+
 end 
 
