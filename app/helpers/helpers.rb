3683e98f21c8e6495eb3ac7c370497a61631789f
Christopher Dent <christopherdent01@gmail.com>
2019-12-14 02:58:15 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -2,13 +2,14 @@ class Helpers < ActiveRecord::Base
   
   
   def self.current_user(session_hash)
-    if session.has_key?(user_id) 
      User.find(session_hash["user_id"])
-    end 
+      
   end
 
   def self.is_logged_in?(session_hash)
+     if session.has_key?(user_id) 
     !!session_hash["user_id"]
+  end 
   end
   
 end 	 
