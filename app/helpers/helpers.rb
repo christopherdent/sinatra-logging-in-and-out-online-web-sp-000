1e18c3e0bd5614e5ef94595f5cd2429f0f450bcf
Christopher Dent <christopherdent01@gmail.com>
2019-12-14 01:53:11 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -2,7 +2,7 @@ class Helpers < ActiveRecord::Base
   
   
   def self.current_user(session_hash)
-    User.find(session_hash["username"])
+    User.find(session_hash["user_id"])
   end
 
   def self.is_logged_in?(session_hash)
